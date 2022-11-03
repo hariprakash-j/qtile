ce0d382e2901637ae02b4e67b4eb677d8a34c0eb
hariprakash-j <73071214+hariprakash-j@users.noreply.github.com>
2022-11-03 17:44:55 +0530

    cleaned layouts

--- qtile_config/def_layouts.py
+++ qtile_config/def_layouts.py
@@ -10,23 +10,8 @@ layout_conf = {
 
 def generate_layouts() -> list():
     return [
-        # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-        # layout.MonadTall(),
         layout.Max(),
-        # layout.Stack(num_stacks=2),
-        # layout.Bsp(),
-        # layout.Matrix(),
-        # layout.MonadWide(),
-        # layout.RatioTile(),
         layout.MonadTall(**layout_conf),
-        # layout.MonadWide(**layout_conf),
-        # layout.Bsp(**layout_conf),
-        # layout.Matrix(columns=2, **layout_conf),
-        # layout.RatioTile(**layout_conf),
-        # layout.Tile(),
-        # layout.TreeTab(),
-        # layout.VerticalTile(),
-        # layout.Zoomy(),
     ]
 
 def generate_floating_layout():
