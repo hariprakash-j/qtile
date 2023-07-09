8d97f67f04c2b18369f334ce6522b718f867eb7b
hariprakash-j <73071214+hariprakash-j@users.noreply.github.com>
2023-07-09 12:31:38 +0530

    fix: layout

--- qtile_config/widgets.py
+++ qtile_config/widgets.py
@@ -1,127 +1,85 @@
 from libqtile import widget
 from .def_theme import colors
 
-def base(fg='text', bg='dark'): 
-    return {
-        'foreground': colors[fg],
-        'background': colors[bg]
-    }
+
+def base(fg="text", bg="dark"):
+    return {"foreground": colors[fg], "background": colors[bg]}
 
 
 def separator():
     return widget.Sep(**base(), linewidth=6, padding=2)
 
 
-def icon(fg='text', bg='dark', fontsize=18, text="?"):
-    return widget.TextBox(
-        **base(fg, bg),
-        fontsize=fontsize,
-        text=text,
-        padding=1
-    )
+def icon(fg="text", bg="dark", fontsize=18, text="?"):
+    return widget.TextBox(**base(fg, bg), fontsize=fontsize, text=text, padding=1)
 
 
 def powerline(fg="light", bg="dark"):
-    return widget.TextBox(
-        **base(fg, bg),
-        text="", # Icon: nf-oct-triangle_left
-        fontsize=50,
-        padding=-6
-    )
+    return widget.TextBox(**base(fg, bg), text="", fontsize=50, padding=-6)  # Icon: nf-oct-triangle_left
+
 
 def powerline_reverse(fg="light", bg="dark"):
-    return widget.TextBox(
-        **base(fg, bg),
-        text="", # Icon: nf-oct-triangle_right
-        fontsize=30,
-        padding=-1
-    )
+    return widget.TextBox(**base(fg, bg), text="", fontsize=30, padding=-1)  # Icon: nf-oct-triangle_right
 
 
-def workspaces(): 
+def workspaces():
     return [
         widget.GroupBox(
-            **base(fg='light'),
+            **base(fg="light"),
             fontsize=17,
             margin_y=3,
             margin_x=3,
             padding_y=2,
             padding_x=2,
             borderwidth=3,
-            active=colors['active'],
-            inactive=colors['inactive'],
+            active=colors["active"],
+            inactive=colors["inactive"],
             rounded=False,
-            highlight_method='block',
-            urgent_alert_method='block',
-            urgent_border=colors['urgent'],
-            this_current_screen_border=colors['focus'],
-            this_screen_border=colors['grey'],
-            other_current_screen_border=colors['dark'],
-            other_screen_border=colors['dark'],
+            highlight_method="block",
+            urgent_alert_method="block",
+            urgent_border=colors["urgent"],
+            this_current_screen_border=colors["focus"],
+            this_screen_border=colors["grey"],
+            other_current_screen_border=colors["dark"],
+            other_screen_border=colors["dark"],
             disable_drag=True
         ),
-        powerline_reverse('color2', 'dark'),
+        powerline_reverse("color2", "dark"),
         separator(),
-        widget.WindowName(**base(fg='focus'), fontsize=15, padding=2),
+        widget.WindowName(**base(fg="focus"), fontsize=15, padding=2),
         separator(),
     ]
 
+
 primary_widgets = [
     *workspaces(),
-
     separator(),
-
-    powerline('color2', 'dark'),
-
-    powerline('dark', 'color2'),
-
-    widget.Memory(**base(bg='dark', fg='light'), measure_mem='G'),
-
-    powerline('color2', 'dark'),
-
-    widget.CPU(**base(bg='color2', fg='dark'), format='{freq_current}GHz {load_percent}%'),
-
-    powerline('dark', 'color2'),
-
-    icon(bg="dark", fg="light", text=' '),
-
-    widget.Volume(**base(bg='dark', fg='light'), step=1),
-
-    powerline('color2', 'dark'),
-    
-    widget.Net(**base(bg='color2'), prefix='M', interface='enp6s0', format='U:{up} D:{down}'),
-
-    powerline('dark', 'color2'),
-
-    icon(bg="dark", fg='light', text=' '), # Icon: nf-fae-sun_cloud
-
-    widget.Wttr(**base(bg='dark', fg='light'), location={'Bangalore': 'Bangalore'}, format="%t"),
-
-    powerline('color2', 'dark'),
-
-    widget.Clock(**base(bg='color2'), format='%A %d/%m - %H:%M'),
-
-    powerline('dark', 'color2'),
-
-    widget.Systray(background=colors['dark'], padding=5),
+    powerline("color2", "dark"),
+    powerline("dark", "color2"),
+    widget.Memory(**base(bg="dark", fg="light"), measure_mem="G"),
+    powerline("color2", "dark"),
+    widget.CPU(**base(bg="color2", fg="dark"), format="{freq_current}GHz {load_percent}%"),
+    powerline("dark", "color2"),
+    icon(bg="dark", fg="light", text=" "),  # Icon: nf-fae-sun_cloud
+    widget.Wttr(**base(bg="dark", fg="light"), location={"Bangalore": "Bangalore"}, format="%t"),
+    powerline("color2", "dark"),
+    widget.Net(**base(bg="color2"), prefix="M", interface="enp6s0", format="U:{up} D:{down}"),
+    powerline("dark", "color2"),
+    icon(bg="dark", fg="light", text=" "),
+    widget.Volume(**base(bg="dark", fg="light"), step=1),
+    powerline("color2", "dark"),
+    widget.Clock(**base(bg="color2"), format="%A %d/%m - %H:%M"),
+    powerline("dark", "color2"),
+    widget.Systray(background=colors["dark"], padding=5),
 ]
 
 secondary_widgets = [
     *workspaces(),
-
     separator(),
-
-    powerline('color2', 'dark'),
-
-    powerline('dark', 'color2'),
-    
-    icon(bg="dark", fg='light', text=' '), # Icon: nf-fae-sun_cloud
-
-    widget.Wttr(**base(bg='dark', fg='light'), location={'Bangalore': 'Bangalore'}, format="%t"),
-
-    powerline('color2', 'dark'),
-
-    widget.Clock(**base(bg='color2'), format='%A %d/%m - %H:%M'),
-
+    powerline("color2", "dark"),
+    powerline("dark", "color2"),
+    icon(bg="dark", fg="light", text=" "),  # Icon: nf-fae-sun_cloud
+    widget.Volume(**base(bg="dark", fg="light"), step=1),
+    powerline("color2", "dark"),
+    widget.Clock(**base(bg="color2"), format="%A %d/%m - %H:%M"),
 ]
-
